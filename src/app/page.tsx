bc2d1a52896cc6e5e32b1c500c39dcc8488d235c
mortytheplatypus <alamrubilmaruf412@gmail.com>
2023-08-12 18:36:16 +0600

    added trending apartment to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,130 +26,218 @@ function Copyright(props:any) {
 }
 
 export default function Home() {
-  const imgSize = 150, wdt = "30vw";
+  const imgSize = 160, wdt = "30vw";
   return (
     <>
       <AppBarComponent>
       </AppBarComponent>
       
       <Grid>  
-        <div>
+        <Grid>
         <Image 
             src="/dhaka-view.jpg" 
             width={1365} height={416}
              
             alt="Apartment"/> 
-        </div>
+        </Grid>
       </Grid>
-
-      <Grid container style={{height:"77vh", backgroundColor: "#D8D8D8"}}>
-        <Grid item style={{width:"75vw", backgroundColor: "#D8D8D8"}}>
-          <Grid>
-            <div style={{backgroundColor: "#D8D8D8"}}>
-              <div style={{display: "flex", justifyContent: "center", backgroundColor: "#D8D8D8", fontSize: "25px", fontFamily: "Tahoma"}}>
-                <h1>Trending Neighbourhood</h1>
-              </div>
-            </div>
+      
+      <Grid container >
+        <Card style={{height:"76.8vh", backgroundColor: "#D8D8D8", width:"75vw", marginRight: "0.4vw", marginLeft: "0.4vw"}}>
+          <Grid style={{display: "flex", justifyContent: "center", backgroundColor: "#D8D8D8", fontSize: "20px", fontFamily: "Tahoma"}}>
+            <h1>Trending Neighbourhood</h1>
           </Grid>
 
-          <Grid container>
+          <Grid container style={{height:"32.5vh"}}>
             <Grid item xs>
-              <Card style={{backgroundColor: "#D8D8D8"}}>
-                <div style={{display: "flex", justifyContent: "right", width:wdt, margin:"20px",  marginLeft:"45px"}}>
-                  <a href="https://en.wikipedia.org/wiki/Dhanmondi_Thana">
-                    <div style={{
-                      display: "flex", justifyContent: "left", backgroundColor: "#F2F2F2",  borderRadius:"10px", fontFamily:"Tahoma", width:wdt}}>
-                      <div>
-                        <Image 
-                          src="/dhanmondi.jpg" 
-                          width={imgSize} height={imgSize}
-                          alt="Dhanmondi"/>   
-                      </div>
-                      <div style={{marginLeft:"40px"}}>
-                        <h1>Dhanmondi</h1>
-                      </div>
-                    </div>  
-                  </a>
-                </div>
+              <Card style={{display: "flex", justifyContent: "right", width:wdt, marginRight:"20px", marginTop:"10px", marginBottom:"0px", marginLeft:"45px", borderRadius:"12px"}}>
+                <Link href="https://en.wikipedia.org/wiki/Dhanmondi_Thana">
+                  <Grid style={{display: "flex", justifyContent: "left", width:wdt}}>
+                    <Grid>
+                      <Image 
+                        src="/dhanmondi.jpg" 
+                        width={imgSize} height={imgSize}
+                        alt="Dhanmondi"/>   
+                    </Grid>
+                    <Grid style={{marginLeft:"40px", fontFamily: "Tahoma"}}>
+                      <h2>Dhanmondi</h2>
+                    </Grid>
+                  </Grid>  
+                </Link>
               </Card>
             </Grid>
 
             <Grid item xs>
-              <Card style={{backgroundColor: "#D8D8D8"}}>
-                <div style={{display: "flex", justifyContent: "right", width:wdt, margin:"20px",  marginLeft:"55px"}}>
-                  <a href="https://en.wikipedia.org/wiki/Motijheel_Thana">
-                    <div style={{
-                        display: "flex", justifyContent: "left", backgroundColor: "#F2F2F2", borderRadius:"10px", fontFamily:"Tahoma", width:wdt}}>
-                      <div>
-                        <Image 
-                            src="/motijheel.jpg" 
-                            width={imgSize} height={imgSize}
-                            alt="Motijheel"/>   
-                      </div>
-                      <div style={{marginLeft:"40px"}}>
-                        <h1>Motijheel</h1>
-                      </div>
-                    </div>
-                  </a>
-                </div>
+              <Card style={{display: "flex", justifyContent: "right", width:wdt, marginRight:"20px", marginTop:"10px", marginBottom:"0px", marginLeft:"55px", borderRadius:"12px"}}>
+                <Link href="https://en.wikipedia.org/wiki/Motijheel_Thana">
+                  <Grid style={{display: "flex", justifyContent: "left", width:wdt}}>
+                    <Grid>
+                      <Image 
+                          src="/motijheel.jpg" 
+                          width={imgSize} height={imgSize}
+                          alt="Motijheel"/>   
+                    </Grid>
+                    <Grid style={{marginLeft:"40px", fontFamily: "Tahoma"}}>
+                      <h2>Motijheel</h2>
+                    </Grid>
+                  </Grid>
+                </Link>
               </Card>
             </Grid>
           </Grid>   
 
-          <Grid container>
+          <Grid container style={{height:"32.5vh"}}>
             <Grid item xs>
-              <Card style={{backgroundColor: "#D8D8D8"}}>
-                <div style={{display: "flex", justifyContent: "right", width:wdt, margin:"20px",  marginLeft:"45px"}}>
-                  <a href="https://en.wikipedia.org/wiki/Azimpur,_Dhaka">
-                    <div style={{
-                      display: "flex", justifyContent: "left", backgroundColor: "#F2F2F2",  borderRadius:"10px", fontFamily:"Tahoma", width:wdt}}>
-                      <div>
-                        <Image 
-                          src="/azimpur.jpg" 
-                          width={imgSize} height={imgSize}
-                          alt="Azimpur"/>   
-                      </div>
-                      <div style={{marginLeft:"40px"}}>
-                        <h1>Azimpur</h1>
-                      </div>
-                    </div>  
-                  </a>
-                </div>
+              <Card style={{display: "flex", justifyContent: "right", width:wdt, marginRight:"20px", marginTop:"0px", marginBottom:"10px", marginLeft:"45px", borderRadius:"12px"}}>
+                <Link href="https://en.wikipedia.org/wiki/Azimpur,_Dhaka">
+                  <Grid style={{ display: "flex", justifyContent: "left", width:wdt}}>
+                    <Grid>
+                      <Image 
+                        src="/azimpur.jpg" 
+                        width={imgSize} height={imgSize}
+                        alt="Azimpur"/>   
+                    </Grid>
+                    <Grid style={{marginLeft:"40px", fontFamily: "Tahoma"}}>
+                      <h2>Azimpur</h2>
+                    </Grid>
+                  </Grid>  
+                </Link>
               </Card>
             </Grid>
 
             <Grid item xs>
-              <Card style={{backgroundColor: "#D8D8D8"}}>
-                  <div style={{display: "flex", justifyContent: "right", width:wdt, margin:"20px",  marginLeft:"55px"}}>
-                    <a href="https://en.wikipedia.org/wiki/Gulshan_Thana">
-                      <div style={{
-                          display: "flex", justifyContent: "left", backgroundColor: "#F2F2F2", borderRadius:"10px", fontFamily:"Tahoma", width:wdt}}>
-                        <div>
-                          <Image 
-                              src="/gulshan.jpg" 
-                              width={imgSize} height={imgSize}
-                              alt="Gulshan"/>   
-                        </div>
-                        <div style={{marginLeft:"40px"}}>
-                          <h1>Gulshan</h1>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
+              <Card style={{display: "flex", justifyContent: "right", width:wdt, marginRight:"20px", marginTop:"0px", marginBottom:"10px", marginLeft:"55px", borderRadius:"12px"}}>
+                <Link href="https://en.wikipedia.org/wiki/Gulshan_Thana">
+                  <Grid style={{display: "flex", justifyContent: "left", width:wdt}}>
+                    <Grid>
+                      <Image 
+                          src="/gulshan.jpg" 
+                          width={imgSize} height={imgSize}
+                          alt="Gulshan"/>   
+                    </Grid>
+                    <Grid style={{marginLeft:"40px", fontFamily: "Tahoma"}}>
+                      <h2>Gulshan</h2>
+                    </Grid>
+                  </Grid>
+                </Link>
               </Card>
             </Grid>
           </Grid>  
-        </Grid>
+        </Card>
 
-        <Grid item style={{width:"23.75vw", backgroundColor: "red"}}>
-          <Grid>
-            <div style={{backgroundColor: "#D8D8D8"}}>
-              <div style={{display: "flex", justifyContent: "center", backgroundColor: "#D8D8D8", fontSize: "25px", fontFamily: "Tahoma"}}>
-                <h1>Explore</h1>
-              </div>
-            </div>
+        <Card style={{height:"76.8vh", backgroundColor: "#D8D8D8", width:"22.5vw", marginRight: "0.4vw"}}>
+          <Grid style={{display: "flex", justifyContent: "center", fontSize: "20px", fontFamily: "Tahoma"}}>
+            <h1>Explore</h1>
           </Grid>
-        </Grid>
+
+          <Grid style={{fontFamily: "Tahoma", margin:"10px"}}>
+            <Card style={{height:"5vw", margin:"10px"}}>
+              <Grid container>
+              <Grid item xs>
+                <Grid style={{fontSize:"20px", marginLeft:"5px"}}>
+                  <b>BDT 15000</b>
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Area: Badda
+                </Grid>
+              </Grid>
+
+              <Grid item xs>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bedroom: 3
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bathroom: 2
+                </Grid>
+              </Grid>
+              </Grid>
+            </Card>
+            <Card style={{height:"5vw", margin:"10px"}}>
+              <Grid container>
+              <Grid item xs>
+                <Grid style={{fontSize:"20px", marginLeft:"5px"}}>
+                  <b>BDT 15000</b>
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Area: Badda
+                </Grid>
+              </Grid>
+
+              <Grid item xs>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bedroom: 3
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bathroom: 2
+                </Grid>
+              </Grid>
+              </Grid>
+            </Card>
+            <Card style={{height:"5vw", margin:"10px"}}>
+              <Grid container>
+              <Grid item xs>
+                <Grid style={{fontSize:"20px", marginLeft:"5px"}}>
+                  <b>BDT 15000</b>
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Area: Badda
+                </Grid>
+              </Grid>
+
+              <Grid item xs>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bedroom: 3
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bathroom: 2
+                </Grid>
+              </Grid>
+              </Grid>
+            </Card>
+            <Card style={{height:"5vw", margin:"10px"}}>
+              <Grid container>
+              <Grid item xs>
+                <Grid style={{fontSize:"20px", marginLeft:"5px"}}>
+                  <b>BDT 15000</b>
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Area: Badda
+                </Grid>
+              </Grid>
+
+              <Grid item xs>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bedroom: 3
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bathroom: 2
+                </Grid>
+              </Grid>
+              </Grid>
+            </Card>
+            <Card style={{height:"5vw", margin:"10px"}}>
+              <Grid container>
+              <Grid item xs>
+                <Grid style={{fontSize:"20px", marginLeft:"5px"}}>
+                  <b>BDT 15000</b>
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Area: Badda
+                </Grid>
+              </Grid>
+
+              <Grid item xs>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bedroom: 3
+                </Grid>
+                <Grid style={{fontSize:"16px", marginLeft:"5px"}}>
+                  Bathroom: 2
+                </Grid>
+              </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        </Card>
       </Grid>
 
       <Copyright/>
