9dc384e0a2b82f7814fbf06ccb61368361285724
Tulu <1805047@ugrad.cse.buet.ac.bd>
2023-08-13 22:58:13 +0600

    authentication header added in advanced search

--- src/app/advance-search/apis.ts
+++ src/app/advance-search/apis.ts
@@ -1,12 +1,17 @@
 import { apiUrls } from '@/lib/apiUrls';
 import axios from 'axios';
 
+import { getCookie } from 'cookies-next';
+
 
 export const searchApartments = async (data: any) => {
 	const res = await axios.get(
         apiUrls.apartments.list,
         {
-            params: data
+            params: data,
+            headers: {
+                Authorization: `${getCookie('token')}`,
+            }
         }
     )
     
