787c9a7d330981ece5c445defee8da23e5007fcf
Tulu <1805047@ugrad.cse.buet.ac.bd>
2023-08-15 20:08:24 +0600

    map bug fixed

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,141 +2,123 @@ import { useEffect, useRef, useMemo, useState } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 import { Typography } from "@mui/material";
 import { _pageHeight } from "@/static/constants";
+import { mapLoader } from "@/services/google-map-service";
 
 export default function Map(props: any) {
-  const address = props.address;
+  let address = props.address;
   const lat = props.lat ? props.lat : 23.8103;
   const lng = props.lng ? props.lng : 90.4125;
+  let fromAddress = props.fromAddress;
+  let fromLatLng = props.fromLatLng;
 
-  const mapRef = useRef<any>(null);
-
-  const loader: any = new Loader({
-    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    version: "weekly",
-  });
-
+  const mapRef = useRef(null);
   useEffect(() => {
-    loader.load().then((google: any) => {
-      let geocoder = new google.maps.Geocoder();
-      geocoder.geocode({ address: address }, (results: any, status: any) => {
-        let lat = results ? results[0].geometry.location.lat() : 23.8103;
-        let lng = results ? results[0].geometry.location.lng() : 90.4125;
-        const map = new google.maps.Map(mapRef.current, {
-          center: {
-            lat,
-            lng,
-          },
-          zoom: 16,
+    if (fromAddress) {
+      const loader = mapLoader;
+
+      loader.importLibrary("geocoding").then(({ Geocoder }) => {
+        // console.log("geocoder", geocoder);
+        let geocoder = new Geocoder();
+        geocoder.geocode({ address }, (results: any, status) => {
+          const _lat = results ? results[0].geometry.location.lat() : 23.8103;
+          const _lng = results ? results[0].geometry.location.lng() : 90.4125;
+          loader.importLibrary("maps").then(({ Map, InfoWindow }) => {
+            const map = new Map(mapRef.current, {
+              center: {
+                lat: _lat,
+                lng: _lng,
+              },
+              zoom: 16,
+            });
+            if (!results) return;
+            let infowindow = new InfoWindow();
+
+            // map.addListener("drag", () => {
+            //   console.log("dragging");
+            // });
+
+            const image = "/icons8-house-25.png";
+            // "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+            loader.importLibrary("marker").then(({ Marker }) => {
+              const marker = new Marker({
+                map: map,
+                position: {
+                  lat: _lat,
+                  lng: _lng,
+                },
+                draggable: props.draggable,
+                icon: image,
+                // label: "H",
+                animation: google.maps.Animation.DROP,
+              });
+              marker.addListener("dragend", (event: any) => {
+                let _lat: number = +event.latLng.lat();
+                let _lng: number = +event.latLng.lng();
+                geocoder.geocode(
+                  { location: { lat: _lat, lng: _lng } },
+                  (results: any, status: any) => {
+                    let address = results[0].address_components
+                      .map((x: any) => x.long_name)
+                      .join(",");
+
+                    infowindow.setContent(address);
+                    infowindow.open(map, marker);
+                    props.setAddress(address);
+                    props.setLatLng({ lat: _lat, lng: _lng });
+                  }
+                );
+
+              });
+            });
+          });
         });
-        if (
-          (!props.lat || !props.lng) &&
-          (!props.address || props.address === "")
-        )
-          return;
-        let infowindow = new google.maps.InfoWindow();
-
-        // map.addListener("drag", () => {
-        //   console.log("dragging");
-        // });
-
-        const image = "/icons8-house-25.png";
-        // "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-
-        const marker = new google.maps.Marker({
-          map: map,
-          position: {
-            lat,
-            lng,
-          },
-          draggable: props.draggable,
-          icon: image,
-          // label: "H",
-          animation: google.maps.Animation.DROP,
-        });
-        marker.addListener("dragend", (event: any) => {
-          let _lat: number = +event.latLng.lat();
-          let _lng: number = +event.latLng.lng();
-          geocoder.geocode(
-            { location: { lat: _lat, lng: _lng } },
-            (results : any, status : any) => {
-              let address = results[0].address_components
-                .map((x: any) => x.long_name)
-                .join(",");
-
-              infowindow.setContent(address);
-              infowindow.open(map, marker);
-              props.setAddress(address);
-              props.setLatLng({ lat: _lat, lng: _lng })
-            }
-          );
-        });
-        // console.log(results[0])
-
-        infowindow.setContent(
-          results[0].address_components.map((x: any) => x.long_name).join(",")
-        );
-        infowindow.open(map, marker);
-        // if (status === "OK") {
-        //   const map = new google.maps.Map(mapRef.current, {
-        //     center: {
-        //       lat,
-        //       lng,
-        //     },
-        //     zoom: 16,
-        //   });
-        //   const marker = new google.maps.Marker({
-        //     map: map,
-        //     position: results[0].geometry.location,
-        //   });
-        // } else {
-        //   console.error(
-        //     `Geocode was not successful for the following reason: ${status}`
-        //   );
-        // }
       });
-    });
+    }
   }, [address]);
 
   useEffect(() => {
-    loader.load().then((google : any) => {
-      let geocoder = new google.maps.Geocoder();
-      geocoder.geocode({ location: { lat, lng } }, (results: any, status : any) => {
-        const map = new google.maps.Map(mapRef.current, {
-          center: {
-            lat,
-            lng,
-          },
-          zoom: 16,
+    if (fromLatLng) {
+      const loader = mapLoader;
+      loader.load().then((google) => {
+        let geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ location: { lat, lng } }, (results: any, status) => {
+          const map = new google.maps.Map(mapRef.current, {
+            center: {
+              lat,
+              lng,
+            },
+            zoom: 16,
+          });
+          if (!props.lat || !props.lng) return;
+          let infowindow = new google.maps.InfoWindow();
+
+          // map.addListener("drag", () => {
+          //   console.log("dragging");
+          // });
+
+          const image = "/icons8-house-25.png";
+          // "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+          const marker = new google.maps.Marker({
+            map: map,
+            position: {
+              lat,
+              lng,
+            },
+            draggable: props.draggable,
+            icon: image,
+            // label: "H",
+            animation: google.maps.Animation.DROP,
+          });
+          // console.log(results[0])
+
+          infowindow.setContent(
+            results[0].address_components.map((x) => x.long_name).join(",")
+          );
+          infowindow.open(map, marker);
         });
-        if (!props.lat || !props.lng) return;
-        let infowindow = new google.maps.InfoWindow();
-
-        // map.addListener("drag", () => {
-        //   console.log("dragging");
-        // });
-
-        const image = "/icons8-house-25.png";
-        // "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-
-        const marker = new google.maps.Marker({
-          map: map,
-          position: {
-            lat,
-            lng,
-          },
-          draggable: props.draggable,
-          icon: image,
-          // label: "H",
-          animation: google.maps.Animation.DROP,
-        });
-        // console.log(results[0])
-
-        infowindow.setContent(
-          results[0].address_components.map((x : any) => x.long_name).join(",")
-        );
-        infowindow.open(map, marker);
       });
-    });
+    }
   }, [lat, lng]);
   return (
     <div
