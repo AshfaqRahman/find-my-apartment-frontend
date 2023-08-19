ee1da479ea894875039a9bffb300ddc8b52b908f
mortytheplatypus <alamrubilmaruf412@gmail.com>
2023-08-19 16:34:02 +0600

    finally merge conflict resolved and working again

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Image from 'next/image'
 import { LoginApi } from './apis';
-import { redirect } from 'next/navigation';
 
-function Copyright(props: any) {
+import { useRouter } from "next/navigation";
+import { _color, _pageHeight, _pageMargin } from "@/static/constants";
+import LoaderComponent from "@/components/loader";
+import ToastComponent from "@/mui-components/toast";
+import { setCookie } from "cookies-next";
+
+function Copyright(props:any) {
   return (
     <Grid align="center" style={{backgroundColor:"E6E6E6", fontSize:"14px"}} {...props}>
       {'Copyright © '}
       <Link color="inherit" href="/">
         Find My Apartment
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
     </Grid>
   );
@@ -30,9 +29,11 @@ function Copyright(props: any) {
 
 export default function Login() {
   const { push } = useRouter();
+
+  var [email, setEmail] = React.useState("")
+  var [password, setPassword] = React.useState("")
+
   let [loggingLoader, setLoggingLoader] = React.useState(false);
-  var [email, setEmail] = React.useState("");
-  var [password, setPassword] = React.useState("");
   let [openError, setOpenError] = React.useState(false);
   let [openSuccess, setOpenSuccess] = React.useState(false);
   let [message, setMessage] = React.useState("");
@@ -42,9 +43,10 @@ export default function Login() {
     const data = {
       email: email,
       password: password,
-    };
-    const response: any = await LoginApi(data);
-    if (response.success) {
+    }
+    // console.log(data);
+    const response : any = await LoginApi(data)
+    if(response.success){
       // console.log(response);
       setMessage("Login Successful");
       setOpenSuccess(true);
@@ -65,74 +67,76 @@ export default function Login() {
   return (
     <>
       <Grid container style={{backgroundColor: "#E6E6E6", height: "96.5vh"}}>
+        <LoaderComponent loading={loggingLoader} />
+        
         <Grid item xs>
           <Grid style={{display: "flex", justifyContent: "right", padding: 10}}>
             <Image 
               src="/skyline.jpg" 
-              width={600} height={600} 
+              width={500} height={500} 
               alt="Skyline"/>
           </Grid>
         </Grid>
         
         <Grid item xs style={{display:"flex", justifyContent:"center"}}>
-            <Grid style={{width:"30vw"}}>
-              <Box sx={{marginTop: 10, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <Grid style={{fontFamily:"Tahoma", fontSize:"40px"}}>
-                  <p>Find My Apartment</p>
-                </Grid>
+          <Grid style={{width:"30vw"}}>
+            <Box sx={{marginTop: 10, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <Grid style={{fontFamily:"Tahoma", fontSize:"40px"}}>
+                <p>Find My Apartment</p>
+              </Grid>
 
-                <Image src="/login-icon.png" width={80} height={80} alt="Login"/>
-                
-                <Box>
-                  <TextField margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    value={email}
-                    onChange={(event:any) => setEmail(event.target.value)}
-                    autoComplete="email"
-                    autoFocus
-                  />
-                  <TextField margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(event:any) => setPassword(event.target.value)}
-                    autoComplete="current-password"
-                  />
-                  <Button
-                    type="submit"
-                    onClick={loginSubmit}
-                    fullWidth
-                    variant="contained"
-                    sx={{mt: 2, mb: 2 }}
-                  >
-                    Login
-                  </Button>
+              <Image src="/login-icon.png" width={80} height={80} alt="Login"/>
+              
+              <Box>
+                <TextField margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  value={email}
+                  onChange={(event:any) => setEmail(event.target.value)}
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(event:any) => setPassword(event.target.value)}
+                  autoComplete="current-password"
+                />
+                <Button
+                  type="submit"
+                  onClick={loginSubmit}
+                  fullWidth
+                  variant="contained"
+                  sx={{mt: 2, mb: 2 }}
+                >
+                  Login
+                </Button>
 
-                  <Grid container>
-                    <Grid item xs>
-                      <Link href="/recover"  style={{display:"flex", fontSize:"14px"}}>
-                        Forgot password?
-                      </Link>
-                    </Grid>
-                    <Grid item>
-                      <Link href="/register" style={{display:"flex", fontSize:"14px"}}>
-                        Don't have an account? Register
-                      </Link>
-                    </Grid>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/recover"  style={{display:"flex", fontSize:"14px"}}>
+                      Forgot password?
+                    </Link>
                   </Grid>
-                </Box>
+                  <Grid item>
+                    <Link href="/register" style={{display:"flex", fontSize:"14px"}}>
+                      Don't have an account? Register
+                    </Link>
+                  </Grid>
+                </Grid>
               </Box>
-            </Grid>
+            </Box>
           </Grid>
-      
+        </Grid>
+      </Grid>
       
       <ToastComponent
         message={message}
@@ -148,8 +152,6 @@ export default function Login() {
         onCross={setOpenSuccess}
         severity="success"
       />
-      </Grid>
-      
       <Copyright/>
     </>
   );
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,11 +6,14 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
 import { createTheme } from '@mui/material/styles';
 import SelectComponent from '@/mui-components/select';
 import { RegisterApi } from './apis';
+import Image from 'next/image';
+import { useRouter } from "next/navigation";
+import { _color, _pageHeight } from "@/static/constants";
+import LoaderComponent from "@/components/loader";
+import ToastComponent from "@/mui-components/toast";
 
 function Copyright(props: any) {
   return (
@@ -23,12 +26,11 @@ function Copyright(props: any) {
     </Grid>
   );
 }
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
 const defaultTheme = createTheme();
 
-export default function SignUp() {
+export default function Register() {
+  const { push } = useRouter();
+
   var [firstName, setFirstName] = React.useState("")
   var [lastName, setLastName] = React.useState("")
   var [email, setEmail] = React.useState("")
@@ -36,7 +38,13 @@ export default function SignUp() {
   var [phone_no, setPhoneNo] = React.useState("")
   var [gender, setGender] = React.useState("")
 
+  let [registerLoading, setRegisterLoading] = React.useState(false);
+  let [openError, setOpenError] = React.useState(false);
+  let [openSuccess, setOpenSuccess] = React.useState(false);
+  let [message, setMessage] = React.useState("");
+
   let registerSubmit = async () => {
+    setRegisterLoading(true);
     const data = {
       first_name: firstName,
       last_name: lastName,
@@ -49,117 +57,151 @@ export default function SignUp() {
     
     if (response.success) {
       // redirect
-      
+      setMessage("Sign Up Successful");
+      setOpenSuccess(true);
+      setTimeout(() => {
+        setRegisterLoading(false);
+        push("/login");
+      }, 1000);
+    } else {
+      setRegisterLoading(false);
+      setMessage(response.message);
+      setOpenError(true);
     }
   };
 
   return (
-    <Grid container style={{backgroundColor: "#D8D8D8", display:"flex", justifyContent:"center"}}>
-      <Grid item maxWidth={"30vw"}>
-        <Box
-          sx={{
-            marginTop: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box>
-            <Grid container spacing={2}>
-              <Grid item md={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
+    <>
+      <Grid container style={{backgroundColor: "#E6E6E6", height:"96.5vh"}}>
+        <LoaderComponent loading={registerLoading} />
+        <Grid item xs>
+          <Grid style={{display: "flex", justifyContent: "right", padding: 10}}>
+            <Image 
+              src="/skyline.jpg" 
+              width={500} height={500} 
+              alt="Skyline"/>
+          </Grid>
+        </Grid>
+          
+        <Grid item xs style={{display:"flex", justifyContent:"center"}}>
+          <Grid style={{width:"30vw"}}>
+            <Box sx={{marginTop: 2, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+
+              <Image src="/register.png" width={150} height={150} alt="Register"/>
+
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item md={6}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      value={firstName}
+                      onChange={(event:any) => setFirstName(event.target.value)}
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item md={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      value={lastName}
+                      onChange={(event:any) => setLastName(event.target.value)}
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      value={email}
+                      onChange={(event:any) => setEmail(event.target.value)}
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      value={password}
+                      onChange={(event:any) => setPassword(event.target.value)}
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                  <Grid item md={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="phone_no"
+                      label="Phone No."
+                      type="tel"
+                      id="phone_no"
+                      value={phone_no}
+                      onChange={(event:any) => setPhoneNo(event.target.value)}
+                      placeholder="01XXX-XXX-XXX"
+                    />
+                  </Grid>
+                  <Grid item md={6}>
+                    <SelectComponent value={gender} elements={['Male', 'Female']} title={'Gender'} handleChange={setGender}/>
+                  </Grid>
+                </Grid>            
+                <Button
+                  type="submit"
+                  onClick={registerSubmit}
                   fullWidth
-                  id="firstName"
-                  label="First Name"
-                  value={firstName}
-                  onChange={(event:any) => setFirstName(event.target.value)}
-                  autoFocus
-                />
-              </Grid>
-              <Grid item md={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  value={lastName}
-                  onChange={(event:any) => setLastName(event.target.value)}
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item md={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  value={email}
-                  onChange={(event:any) => setEmail(event.target.value)}
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item md={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(event:any) => setPassword(event.target.value)}
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item md={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="phone_no"
-                  label="Phone No."
-                  type="tel"
-                  id="phone_no"
-                  value={phone_no}
-                  onChange={(event:any) => setPhoneNo(event.target.value)}
-                  placeholder="01XXX-XXX-XXX"
-                />
-              </Grid>
-              <Grid item md={6}>
-                <SelectComponent value={gender} elements={['Male', 'Female']} title={'Gender'} handleChange={setGender}/>
-              </Grid>
-            </Grid>            
-            <Button
-              type="submit"
-              onClick={registerSubmit}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Register
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Login
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Register
+                </Button>
+                
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/" style={{fontSize:"14px", display:"flex", justifyContent:"left"}}>
+                      Back to Home
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/login" style={{fontSize:"14px", display:"flex", justifyContent:"center"}}>
+                    Already have an account? Login
+                  </Link>
+                </Grid>
+                </Grid>
+              </Box>
+            </Box>  
+          </Grid>        
+        </Grid>
       </Grid>
-    </Grid>
-    
+
+      <Copyright/>
+      
+      <ToastComponent
+        message={message}
+        open={openError}
+        onClose={setOpenError}
+        onCross={setOpenError}
+        severity="error"
+      />
+      <ToastComponent
+        message={message}
+        open={openSuccess}
+        onClose={setOpenSuccess}
+        onCross={setOpenSuccess}
+        severity="success"
+      />
+    </>
   );
 }
