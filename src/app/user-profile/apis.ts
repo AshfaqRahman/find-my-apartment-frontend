6c8a7f9aa7e48648616855a1a2af39c7b8951909
Tulu <1805047@ugrad.cse.buet.ac.bd>
2023-08-20 19:32:35 +0600

    get user profile info api added

--- src/app/user-profile/apis.ts
+++ src/app/user-profile/apis.ts
@@ -3,17 +3,33 @@ import axios from 'axios';
 import { getCookie } from 'cookies-next';
 
 
-export const searchApartments = async (data: any) => {
-	const res = await axios.get(
-        apiUrls.apartments.list,
-        {
-            params: data
+export const getUserData = async () => {
+	
+
+    try {
+        console.log("getUserData:: ");
+
+        const res = await axios.get(
+            apiUrls.user.info,
+            {
+                headers: {
+                    Authorization: `${getCookie('token')}`
+                }
+            }
+        )
+
+        return { data: res.data, success: res.status === 200 }
+
+    } catch (error: any) {
+        console.log("getUserData :: error", error);
+        if (error.response.status === 500) {
+            return {
+                success: false,
+                message: error.response.data.message,
+            }
         }
-    )
+        // return error.response.data
+    }
     
     return res.data
 };
-
-export const find = async (data) => {
-    
-}
