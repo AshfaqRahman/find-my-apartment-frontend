1bdbba2e848cf38c689f6e0896dbb15535c3f4c6
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2023-09-10 21:15:19 +0600

    chat ui added with socket io

---
+++ src/app/chat/apis.tsx
@@ -0,0 +1,51 @@
+import { apiUrls } from '@/lib/apiUrls';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+
+// call api/message/chatlist to fetch chat list
+export const getChatList = async () => {
+  const token = getCookie('token');
+  const response = await axios.get(apiUrls.chat.list, {
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+  //console.log("getChatList", response);
+  if(response.status !== 200) {
+    return {
+      success: false,
+      message: "Something went wrong",
+      data: [],
+    }
+  }
+  return {
+    success: true,
+    message: "Chat list fetched successfully",
+    data: response.data.data,
+  }
+}
+
+// call api/message/:friend_id to fetch chat
+export const getChat = async (friend_id: string) => {
+  const token = getCookie('token');
+  const response = await axios.get(
+    `${apiUrls.chat.messages}/${friend_id}`, 
+    {
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+ // console.log("getChat", response);
+  if(response.status !== 200) {
+    return {
+      success: false,
+      message: "Something went wrong",
+      data: [],
+    }
+  }
+  return {
+    success: true,
+    message: "Chat fetched successfully",
+    data: response.data.data,
+  }
+}
--- src/lib/apiUrls.ts
+++ src/lib/apiUrls.ts
@@ -27,9 +27,6 @@ export const apiUrls = {
 	},
 
 	user: {
-		upgrade: '/api/user/upgrade',
-		modify: '/api/user',
-		usage: 'api/user/usage',
 		preference: `${HOST}/api/user/my-preference`,
 		info: `${HOST}/api/user/profile`
 	},
@@ -40,30 +37,11 @@ export const apiUrls = {
 		checkAuth: `${HOST}/api/auth/is-token-valid`,
 		
 	},
-	expenses: {
-		add: '/api/expenses/add',
-		modify: '/api/expenses',
-		getExpenses: ({ from, to }: { from: string; to: string }) => `/api/expenses?from=${from}&to=${to}`,
-	},
-	investments: {
-		add: '/api/investments/add',
-		modify: '/api/investments',
-		getInvestments: ({ from, to }: { from: string; to: string }) => `/api/investments?from=${from}&to=${to}`,
-	},
-	income: {
-		add: '/api/income/add',
-		modify: '/api/income',
-		getIncome: ({ from, to }: { from: string; to: string }) => `/api/income?from=${from}&to=${to}`,
-	},
-	subscriptions: {
-		add: '/api/subscriptions/add',
-		modify: '/api/subscriptions',
-		getSubscriptions: ({ from, to }: { from: string; to: string }) => `/api/subscriptions?from=${from}&to=${to}`,
-	},
-	feedback: {
-		add: `/api/feedback`,
-	},
 	rooms: {
 		list: `${HOST}/api/posts`,
+	},
+	"chat": {
+		"list": `${HOST}/api/message/chatlist`,
+		"messages": `${HOST}/api/message/`,
 	}
 };
